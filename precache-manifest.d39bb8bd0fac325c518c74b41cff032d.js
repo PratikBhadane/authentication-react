3747f9c80f77ec1f03add529277a65c492f1a969
Pratik Bhadane <pratikbhadane100@gmail.com>
2020-08-24 09:41:12 +0530

    Updates

---
+++ precache-manifest.d39bb8bd0fac325c518c74b41cff032d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb436bb964c8b3a3df28b704aa8c7643",
+    "url": "/authentication-react/index.html"
+  },
+  {
+    "revision": "effcc3f4ef4a0d3a2546",
+    "url": "/authentication-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bc9582fe261b33bb4264",
+    "url": "/authentication-react/static/css/main.e145122b.chunk.css"
+  },
+  {
+    "revision": "effcc3f4ef4a0d3a2546",
+    "url": "/authentication-react/static/js/2.ef3777de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/authentication-react/static/js/2.ef3777de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9582fe261b33bb4264",
+    "url": "/authentication-react/static/js/main.1f02d6c5.chunk.js"
+  },
+  {
+    "revision": "40e43049dbb35cf0305c",
+    "url": "/authentication-react/static/js/runtime-main.92753a49.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/authentication-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
